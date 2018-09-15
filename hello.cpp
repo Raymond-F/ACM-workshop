19a6f547786a36a4b37d07f227e5bd198341c5fd
Raymond-F <37716203+Raymond-F@users.noreply.github.com>
2018-09-14 18:10:59 -0700

    Update hello.cpp
    Changed from the website

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(){
     cout << "Hello World!" << endl;
     cout << "This masterpiece of a program was made by Raymond Fanning..." << endl;
+    //this is another modification
     return 0;
 }
